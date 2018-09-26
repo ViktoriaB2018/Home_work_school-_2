b33c8fef20352c2be5797df2b0e2a5ab3a024095
ViktoriaB2018 <bvo2008@mail.ru>
2018-09-26 19:33:41 +0300

    Fix bag creating Barbers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_154842) do
+ActiveRecord::Schema.define(version: 2018_09_26_163043) do
 
-  create_table "barber", force: :cascade do |t|
+  create_table "barbers", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
