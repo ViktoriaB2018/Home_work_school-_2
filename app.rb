524161427465de9a7f3d610bbb54bd775debbda7
ViktoriaB2018 <bvo2008@mail.ru>
2018-09-28 10:22:14 +0300

    Add view visit.erb & saving clients  information to db table Clients

--- app.rb
+++ app.rb
@@ -12,7 +12,31 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.order "created_at DESC"
+end
+
+get '/' do
 	erb :index			
 end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+	@select_barber = params[:select_barber]
+	@color = params[:color]
+
+	Client.create [ :name => @username,
+					:phone => @phone,
+					:datestamp => @date_timee,
+					:barber => @select_barber,
+					:color => @color ]
+
+	erb "<h4>Спасибо, Вы записаны!</h4>"
+end
+
